6133bdfa6c251a818b888aa7900ec17f30fd65e1
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-08-24 13:30:19 -0600

    changed database settings

---
+++ products_project/local_setting.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-iac52jo+i!=)8jy%aal91!@w9zut)-%w(^(pe%6&zdr&4d+9s9'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'product_datablase',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSOWORD': 'password'
+    }
+}
